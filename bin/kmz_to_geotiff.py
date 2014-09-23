7ac270dea9c08b305e07367c28e9a97f86e32a79
Brian Coltin <brian.j.coltin@nasa.gov>
2014-09-23 11:59:01 -0700

    KMZ conversion script outputs mosaic strips now (optionally).

--- bin/kmz_to_geotiff.py
+++ bin/kmz_to_geotiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import subprocess
 import sys
 
 import zipfile
@@ -11,6 +12,8 @@ from xml.etree import ElementTree
 if len(sys.argv) < 3:
 	print 'Usage: kmz_to_geotiff.py input.kmz output.tiff'
 	sys.exit(0)
+
+single_mosaic = False
 
 try:
 	z = zipfile.ZipFile(sys.argv[1], 'r')
@@ -36,9 +39,14 @@ except:
 
 tempdir = tempfile.mkdtemp()
 
-input_images = []
-input_image_string = ''
 failure = False
+
+input_images = []
+if single_mosaic:
+	input_image_string = ''
+else:
+	nonblank_images = []
+	prev_lats = (None, None)
 
 for link in links:
 	latlon = link.find('{%s}Region' % namespace).find('{%s}LatLonAltBox' % namespace)
@@ -47,28 +55,67 @@ for link in links:
 	south = latlon.find('{%s}south' % namespace).text
 	east = latlon.find('{%s}east' % namespace).text
 	west = latlon.find('{%s}west' % namespace).text
+
 	output_name = tempdir + os.sep + name + '.tiff'
 	z.extract('images/%s.png' % (name), tempdir)
 	imagefile = tempdir + os.sep + ('images/%s.png' % (name))
-	ret = os.system('gdal_translate -q -a_srs "+proj=longlat +datum=WGS84 +no_defs" -a_ullr %s %s %s %s %s %s' % (west, north, east, south, imagefile, output_name))
+
+	try:
+		out = subprocess.check_output('identify -verbose %s | grep "standard deviation"' % (imagefile), shell=True)
+		blank = False
+		if float(out.split('\n')[0].strip().split()[2]) == 0.0:
+			blank = True
+	except:
+		print >> sys.stderr, "Failed to determine if image %s was blank." % (imagefile)
+		failure = True
+		break
+
+	print 'Converting ' + output_name + '...'
+	if not blank:
+		ret = os.system('gdal_translate -q -b 1 -b 2 -b 3 -mask 4 -co "PHOTOMETRIC=RGB" -a_srs "+proj=longlat +datum=WGS84 +no_defs" -a_ullr %s %s %s %s --config GDAL_TIFF_INTERNAL_MASK YES %s %s' % (west, north, east, south, imagefile, output_name))
 	os.remove(imagefile)
+	if blank:
+		continue
 	if ret != 0:
 		print >> sys.stderr, 'Failed to convert tile %s to geotiff.' % (name)
 		failure = True
 		break
 	input_images.append(output_name)
-	input_image_string += ' ' + output_name
-	print 'Converting ' + output_name + '...'
-
-print 'Merging tiles...'
+	if single_mosaic:
+		input_image_string += ' ' + output_name
+	else:
+		if prev_lats[0] == north and prev_lats[1] == south:
+			nonblank_images[-1].append(output_name)
+		else:
+			nonblank_images.append([output_name])
+			prev_lats = (north, south)
 
 if not failure:
-	ret = os.system('gdal_merge.py -init "0 0 0 0" -o %s %s' % (sys.argv[2], input_image_string))
-	if ret != 0:
-		print >> sys.stderr, 'Merge failed.'
-		failure = True
+	print 'Merging tiles...'
+	if single_mosaic:
+		ret = os.system('gdal_merge.py -init "0 0 0 0" -o %s %s' % (sys.argv[2], input_image_string))
+		if ret != 0:
+			print >> sys.stderr, 'Merge failed.'
+			failure = True
+		else:
+			print 'Merge successful. Output to ' + sys.argv[2] + '.'
 	else:
-		print 'Merge successful. Output to ' + sys.argv[2] + '.'
+		print nonblank_images
+		cur_line = 0
+		while cur_line < len(nonblank_images):
+			cur_images = ''
+			ROWS = 1
+			for i in range(ROWS):
+				if cur_line + i < len(nonblank_images):
+					for a in nonblank_images[cur_line+i]:
+						cur_images += a + ' '
+			cur_line = cur_line + ROWS
+			ret = os.system('gdal_merge.py -o %s_%d.tiff %s' % (sys.argv[2], cur_line / ROWS, cur_images))
+			if ret != 0:
+				print >> sys.stderr, 'Merge failed.'
+				failure = True
+			else:
+				print 'Merge successful. Output to ' + sys.argv[2] + '_' + str(cur_line / ROWS) + '.tiff.'
 
 for i in input_images:
 	os.remove(i)
