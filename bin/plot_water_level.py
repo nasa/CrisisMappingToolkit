350ced753bb708a123b53cc20c76c431e1f180a5
Brian Coltin <brian.j.coltin@nasa.gov>
2014-09-29 15:12:55 -0700

    Plotting lake water level works, for Mono Lake.

---
+++ bin/plot_water_level.py
@@ -0,0 +1,75 @@
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
+import numpy
+
+if len(sys.argv) < 2:
+	print >> sys.stderr, 'Usage: plot_water_level.py water_levels.txt'
+
+f = open(sys.argv[1], 'r')
+
+x_axis = []
+y_axis = []
+
+# take values with low cloud cover
+parts = f.readline().split(',')
+names = parts[0]
+country = parts[1]
+area = parts[2]
+for l in f:
+	parts = l.split()
+	date_parts = parts[0][:-1].split('-')
+	date = datetime.date(int(date_parts[0]), int(date_parts[1]), int(date_parts[2]))
+	cloud = int(parts[1])
+	water = int(parts[3])
+	if cloud < 1000:
+		x_axis.append(date)
+		y_axis.append(water * 0.03 * 0.03) #pixels * km^2 / pixel, km^2 / pixel = 0.03 * 0.03 / 1
+
+f.close()
+
+
+# remove values that differ from neighbors by large amounts
+NEIGHBOR_RADIUS = 3
+OUTLIER_FACTOR = 0.9
+remove = []
+for i in range(len(y_axis)):
+	start = max(0, i - NEIGHBOR_RADIUS)
+	end = min(len(y_axis), i + NEIGHBOR_RADIUS)
+	if i > 0:
+		neighbors = y_axis[start:i-1]
+	else:
+		neighbors = []
+	if i < len(y_axis) - 1:
+		neighbors.extend(y_axis[i+1:end])
+	num_neighbors = end - start - 1
+	num_outliers = 0
+	for v in neighbors:
+		if v < y_axis[i] * OUTLIER_FACTOR or v > y_axis[i] / OUTLIER_FACTOR:
+			num_outliers += 1
+	if float(num_outliers) / num_neighbors >= 0.5:
+		remove.append(i)
+
+for i in reversed(remove):
+	y_axis.pop(i)
+	x_axis.pop(i)
+
+fig, ax = plt.subplots()
+ax.plot(x_axis, y_axis, marker='o', linestyle='--')
+ax.xaxis.set_major_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+ax.xaxis.set_minor_locator(mdates.MonthLocator())
+
+ax.format_xdata = mdates.DateFormatter('%m/%d/%Y')
+ax.format_ydata = (lambda x : '%g km^2' % (x))
+ax.set_xlabel('Time')
+ax.set_ylabel('Lake Surface Area (km^2)')
+fig.suptitle('Mono Lake Surface Area from Landsat')
+
+ax.grid(True)
+fig.autofmt_xdate()
+
+plt.show()
+
--- lake_measure.py
+++ lake_measure.py
@@ -13,19 +13,24 @@ from pprint import pprint
 import ee
 
 def get_image_collection(bounds, start_date, end_date):
-	ee_bounds = apply(ee.Geometry.Rectangle, bounds)
-	ee_points = map(ee.Geometry.Point, [(bounds[0], bounds[1]), (bounds[0], bounds[3]),
-									(bounds[2], bounds[1]), (bounds[2], bounds[3])])
-	collection = ee.ImageCollection('LT5_L1T').filterDate(start_date, end_date).filterBounds(ee_points[0]).filterBounds(ee_points[1]).filterBounds(ee_points[2]).filterBounds(ee_points[3])
+	#ee_bounds = apply(ee.Geometry.Rectangle, bounds)
+	#ee_points = map(ee.Geometry.Point, [(bounds[0], bounds[1]), (bounds[0], bounds[3]),
+	#								(bounds[2], bounds[1]), (bounds[2], bounds[3])])
+	ee_bounds = bounds
+	ee_points = ee.List(bounds.bounds().coordinates().get(0))
+	points = ee_points.getInfo()
+	points = map(functools.partial(apply, ee.Geometry.Point), points)
+	collection = ee.ImageCollection('LT5_L1T').filterDate(start_date, end_date).filterBounds(points[0]).filterBounds(points[1]).filterBounds(points[2]).filterBounds(points[3])
 	return collection
 
 def detect_clouds(im):
 	cloud_mask = im.select(['B3']).gte(35).select(['B3'], ['cloud'])
 	NDSI = (im.select(['B2']).subtract(im.select(['B5']))).divide(im.select(['B2']).add(im.select(['B5'])))
 	# originally 0.4, but this supposedly misses some clouds, used 0.7 in paper
-	cloud_mask = cloud_mask.And(NDSI.lte(0.4))
+	# be conservative
+	#cloud_mask = cloud_mask.And(NDSI.lte(0.7))
 	# should be 300K temperature, what is this in pixel values?
-	cloud_mask = cloud_mask.And(im.select(['B6']).lte(105))
+	cloud_mask = cloud_mask.And(im.select(['B6']).lte(100))
 	return cloud_mask
 
 def detect_water(image, clouds):
@@ -33,15 +38,18 @@ def detect_water(image, clouds):
 	water = image.select(['B1']).lte(100).And(image.select(['B2']).lte(55)).And(image.select(['B3']).lte(71))
 	water = water.select(['B1'], ['water'])
 	# originally B7 <= 13
-	water = water.And(image.select(['B4']).lte(66)).And(image.select(['B5']).lte(47)).And(image.select(['B7']).lte(22))
+	water = water.And(image.select(['B4']).lte(66)).And(image.select(['B5']).lte(47)).And(image.select(['B7']).lte(20))
+	# original was B1 57, 23, 21, 14
+	water = water.And(image.select(['B1']).gte(30))#.And(image.select(['B2']).gte(10)).And(image.select(['B3']).gte(8))
+	#water = water.And(image.select(['B4']).gte(5)).And(image.select(['B5']).gte(2)).And(image.select(['B7']).gte(1))
 	water = water.And(clouds.Not())
 	return water
 
 def count_water(bounds, image):
-	print 'count'
 	clouds = detect_clouds(image)
 	water = detect_water(image, clouds)
 	cloud_count = clouds.mask(clouds).reduceRegion(ee.Reducer.count(), bounds, 30)
+	#addToMap(ee.Algorithms.ConnectedComponentLabeler(water, ee.Kernel.square(1), 256))
 	water_count = water.mask(water).reduceRegion(ee.Reducer.count(), bounds, 30)
 	return ee.Feature(None, {'date' : image.get('DATE_ACQUIRED'),
 			'water_count' : water_count.get('water'),
@@ -56,7 +64,8 @@ parser.add_argument('--date', dest='date', action='store', required=False, defau
 args = parser.parse_args()
 
 if args.date == None:
-	start_date = ee.Date('1984-01-01')
+	#start_date = ee.Date('1984-01-01')
+	start_date = ee.Date('1999-09-20')
 	end_date = ee.Date('2030-01-01')
 else:
 	start_date = ee.Date(args.date)
@@ -66,15 +75,26 @@ else:
 #start_date = ee.Date('1993-07-01') # lake low
 #start_date = ee.Date('1993-06-01') # lake low but some jet streams
 
-bounds = (-119.2, 37.9,-118.85, 38.1)
-collection = get_image_collection(bounds, start_date, end_date)
-ee_bounds = apply(ee.Geometry.Rectangle, bounds)
+#all_lakes = ee.FeatureCollection('ft:13s-6qZDKWXsLOWyN7Dap5o6Xuh2sehkirzze29o3', "geometry").toList(1000000)
+all_lakes = ee.FeatureCollection('ft:13s-6qZDKWXsLOWyN7Dap5o6Xuh2sehkirzze29o3', "geometry").filterMetadata(u'LAKE_NAME', u'equals', u'Mono Lake').toList(1000000)
+lake_num = 0
+lake = ee.Feature(all_lakes.get(lake_num))
+#print lake.get('TYPE').getInfo()
+#print lake.get('name').getInfo()
+#print lake.get('COUNTRY').getInfo()
+#print lake.get('AREA_SKM').getInfo()
+ee_bounds = lake.geometry().buffer(1000)
+
+#bounds = (-119.2, 37.9,-118.85, 38.1)
+#ee_bounds = apply(ee.Geometry.Rectangle, bounds)
+collection = get_image_collection(ee_bounds, start_date, end_date)
 
 # display individual image from a date
 if args.date:
 	from util.mapclient_qt import centerMap, addToMap
 	landsat = ee.Image(collection.first())
-	centerMap((bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2, 11)
+	center = ee_bounds.centroid().getInfo()['coordinates']
+	centerMap(center[0], center[1], 11)
 	addToMap(landsat, {'bands': ['B3', 'B2', 'B1']}, 'Landsat 3,2,1 RGB')
 	addToMap(landsat, {'bands': ['B7', 'B5', 'B4']}, 'Landsat 7,5,4 RGB', False)
 	addToMap(landsat, {'bands': ['B6']}, 'Landsat 6', False)
@@ -82,10 +102,28 @@ if args.date:
 	water = detect_water(landsat, clouds)
 	addToMap(clouds.mask(clouds), {'opacity' : 0.5}, 'Cloud Mask')
 	addToMap(water.mask(water), {'opacity' : 0.5, 'palette' : '00FFFF'}, 'Water Mask')
-	print count_water(ee_bounds, landsat).getInfo()
+	addToMap(ee.Feature(ee_bounds))
+	#print count_water(ee_bounds, landsat).getInfo()
 # compute water levels in all images of area
 else:
-	results = collection.map(functools.partial(count_water, ee_bounds))
+	v = collection.toList(1000000)
+	name = lake.get('LAKE_NAME').getInfo()
+	country = lake.get('COUNTRY').getInfo()
+	area = lake.get('AREA_SKM').getInfo()
+	print '%s, %s, %s' % (name, country, area)
+	results = []
+	for i in range(v.length().getInfo()):
+		im = ee.Image(v.get(i))
+		r = count_water(ee_bounds, im).getInfo()['properties']
+		print '%s: %10d Cloud, %10d Water, %10d Total' % (r['date'], r['cloud_count'], r['water_count'], r['cloud_count'] + r['water_count'])
+		results.append(r)
+	#v = ee.List(collection.iterate(lambda v, l: ee.List(l).add(count_water(ee_bounds, v)), ee.List([])))
+	#print v.get(0).getInfo()
+	#all_images = collection.toList(10000000).getInfo()
+	#for im in all_images:
+	#	print im.keys()
+	#	print count_water(bounds, im)
+	#results = collection.map(functools.partial(count_water, ee_bounds))
 	#print results.toList(1000000).length().getInfo()
 	#print collection.iterate(lambda v, l: l.append(v), [])
 
