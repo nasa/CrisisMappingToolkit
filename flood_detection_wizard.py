8c3cfe15548bb2bd4b23f26c8dfbb16e52b62932
Scott McMichael <scott.t.mcmichael@nasa.gov>
2014-11-25 14:26:09 -0800

    Added instructions to the flood detection wizard

--- flood_detection_wizard.py
+++ flood_detection_wizard.py
@@ -1,5 +1,24 @@
 '''
     Setup script for the "production" flood detection tool.
+    
+    Instructions for using this tool:
+    
+    1 - Run this script.  The GUI should appear.
+    2 - Select the date of the flood you are interested in using
+        the button with the date in the top left corner.
+    3 - Pan and zoom to the region of interest.
+    4 - Press the 'Set Processing Region' button to set the
+        area where image statistics are calculated.
+    5 - Press the 'Load Images' button to search for Landsat,
+        MODIS, DEM, and cloud mask images for that region/date.
+    6 - Press either 'Detect Flood' button to run the flood detection
+        algorithm on the MODIS image.
+    7 - You can adjust the two slider bars at the bottom to control
+        the sensitivity of the flood detection algorithm:
+        - Change Detection Threshold = Decrease this number to find more flood pixels.
+        - Water Mask Threshold = Increase this number to find more flood pixels.
+    8 - If you want to look at a new area, press the 'Clear Map' button and go back to
+        step 2.
 '''
 
 import logging
