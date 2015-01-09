0d0542b009ade7cc8770b9120ed270587ce2b0b6
Brian Coltin <brian.j.coltin@nasa.gov>
2015-01-08 16:55:23 -0800

    Fixed kashmore ground truth id.

--- cmt/util/evaluation.py
+++ cmt/util/evaluation.py
@@ -44,6 +44,9 @@ def __evaluate_approach(result, ground_truth, region, fractional=False):
     correct_sum = ee.data.getValue({'image': correct.stats(     30000, region, 'EPSG:4326').serialize(), 'fields': 'b1'})['properties']['b1']['values']['sum']
     result_sum  = ee.data.getValue({'image': result.stats(      30000, region, 'EPSG:4326').serialize(), 'fields': 'b1'})['properties']['b1']['values']['sum']
     truth_sum   = ee.data.getValue({'image': ground_truth.stats(30000, region, 'EPSG:4326').serialize(), 'fields': 'b1'})['properties']['b1']['values']['sum']
+    # correct_sum = correct.reduceRegion(ee.Reducer.sum(), region, 30).getInfo()['b1']
+    # result_sum  = result.reduceRegion(ee.Reducer.sum(), region, 30).getInfo()['b1']
+    # truth_sum = ground_truth.reduceRegion(ee.Reducer.sum(), region, 30).getInfo()['b1']
     precision   = 1.0 if (result_sum == 0.0) else (correct_sum / result_sum)
     recall      = 1.0 if (truth_sum  == 0.0) else (correct_sum / truth_sum)
     return (precision, recall)
