019dad5b3f99bf34f85028d89d6d421b8a387226
Brian Coltin <brian.j.coltin@nasa.gov>
2015-02-04 12:42:23 -0800

    Use new EE authentication method.

--- cmt/ee_authenticate.py
+++ cmt/ee_authenticate.py
@@ -27,10 +27,15 @@ __MY_PRIVATE_KEY_FILE = expanduser('~/.local/google_service_api_private_key.pem'
 
 def initialize(account=None, key_file=None):
     '''Initialize the Earth Engine object, using your authentication credentials.'''
-    if account == None:
-        f = open(__MY_ACCOUNT_FILE, 'r')
-        account = f.readline().strip()
-    if key_file == None:
-        key_file = __MY_PRIVATE_KEY_FILE
-    ee.Initialize(ee.ServiceAccountCredentials(account, key_file))
+    try:
+        ee.Initialize()
+    except:
+        # in the past, EE keys had to be installed manually. We keep this old method for
+        # backwards compatibility
+        if account == None:
+            f = open(__MY_ACCOUNT_FILE, 'r')
+            account = f.readline().strip()
+        if key_file == None:
+            key_file = __MY_PRIVATE_KEY_FILE
+        ee.Initialize(ee.ServiceAccountCredentials(account, key_file))
 
